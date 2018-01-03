7bce8ce291cee817091259a33e16044613272d84
asandhuzero <as7687a@student.american.edu>
2018-01-03 17:29:55 -0500

    Run is executed whenever the input field has been changed and is no longer in focus.

--- src/app.js
+++ src/app.js
@@ -16,10 +16,9 @@ Example:    LDI R26,0x20
             LDI R30,0x60
             LD R4,-Z         ;Load R4 with loc. 0x5F 
     `;
-    this.output = "placeholder";
+    this.output = 'placeholder';
     this.assemblyCodeArray = [];
     this.programCounter = 0;
-
     this.reader = new Reader();
     this.evaluator = new Evaluator();
 
@@ -37,34 +36,40 @@ Example:    LDI R26,0x20
   // so I wanted to just make this a funciton and call it within the function.
   // Although what I really want to do is get some sort of logic flow that
   // within this program.
-  Update() {
+  // This really should take program counter
+  Update(programCounter) {
+    this.setProgramCounter(programCounter);
     let new_output = this.evaluator.Evaluate(this.assemblyCodeArray, this.programCounter);
     this.setOutput(new_output);
   }
   Run() {
-    let executedProgramCounter = this.assemblyCodeArray.length - 1;
-    this.setProgramCounter(executedProgramCounter);
     this.assemblyCodeArray = this.reader.Read(this.input);
-    // TODO:
-    // Mostly a note to self. This is the only area where the program will
-    // actually evaluate the code. This could be a problem because what if
-    // the user wishes to see the evaluation per program counter step
-    this.Update();
+    let executedProgramCounter = this.assemblyCodeArray.length-1;
+    this.Update(executedProgramCounter);
+  }
+  Reset() {
+    let executedProgramCounter = 0;
+    this.Update(executedProgramCounter);
   }
   StepBack() {
+    this.assemblyCodeArray = this.reader.Read(this.input);
     let executedProgramCounter = this.programCounter - 1;
-    this.setProgramCounter(executedProgramCounter);
-    this.Update();
+    this.Update(executedProgramCounter);
   }
   StepForward() {
+    this.assemblyCodeArray = this.reader.Read(this.input);
     let executedProgramCounter = this.programCounter + 1;
-    this.setProgramCounter(executedProgramCounter);
-    this.Update();
+    this.Update(executedProgramCounter);
   }
- setProgramCounter(number) {
-   if (this.assemblyCodeArray.length > number 
-    && number >= 0) {
-      this.programCounter = number;  
+  setProgramCounter(number) {
+    if (this.assemblyCodeArray.length - 1 < number) {
+      this.programCounter = this.assemblyCodeArray.length - 1;
+    }
+    else if (number < 0) {
+      this.programCounter = 0;
+    }
+    else {
+      this.programCounter = number;
     }
     console.log(this.programCounter);
   }
