8afff44bcd89bdd89171f869eb1df53724cda161
asandhuzero <as7687a@student.american.edu>
2018-01-08 22:41:59 -0500

    Upon looking more at Aurelia and attempting to decouple the ISA from the app, it might be the best solution to inject the AVR ISA into the application allowing for a more loosely coupled application.

---
+++ src/avr-details.js
@@ -0,0 +1,5 @@
+/**
+ * This is an AVR details file. It will be used to inject into the register
+ * and app components what the AVR Instruction set should be emulating.
+ * As in the register and what data is encapsulated within it.
+ */
