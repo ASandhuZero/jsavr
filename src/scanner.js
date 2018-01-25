11d5469689850158b00b34bccae4e5aa302b4577
asandhuzero <as7687a@student.american.edu>
2018-01-25 00:19:48 -0500

    Removed for loop of token-type creation in Scanner

--- src/scanner.js
+++ src/scanner.js
@@ -1,22 +1,14 @@
 import tokens from 'text!token-type.json'
+
 export class Scanner {
+
 	constructor() {
     this.tokens = JSON.parse(tokens);
-    this.token_reg_exes = {}
-    console.log(this.tokens);
-
-   for (let token_name in this.tokens) {
-     let token_contents = this.tokens[token_name];
-     let reg_ex_string = token_contents["reg_ex"];
-     let reg_ex = new RegExp(reg_ex_string);
-     if (reg_ex.test(" ")) {
-       console.log("This works!");
-     }
-   }
+    this.token_reg_exes = {};
   }
+  
   Scan(token) {
     console.log(token);
-    
   }
 
   Error(line, message) {
