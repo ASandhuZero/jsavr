caee37f4abd7cad1387ea1483dc56704d69cf64d
asandhuzero <as7687a@student.american.edu>
2018-01-31 01:07:27 -0500

    Got some really weak regex objects to actually not crash everything. This might be the way to the solution actually. Got to see if I can lex source text?

--- src/scanner.js
+++ src/scanner.js
@@ -4,7 +4,17 @@ export class Scanner {
 
 	constructor() {
     this.tokens = JSON.parse(tokens);
+    console.log(this.tokens);
+    
     this.token_reg_exes = {};
+    for (let token_key in this.tokens) {
+      let token = this.tokens[token_key];
+      console.log(token);
+      
+      this.token_reg_exes[token["token"]] = new RegExp(token["reg_ex"]);
+    }
+    console.log(this.token_reg_exes);
+    
   }
   
   Scan(token) {
