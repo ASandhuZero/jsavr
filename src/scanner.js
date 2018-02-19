e33e2981b024b21d9a430e03651e32ba277388fb
asandhuzero <as7687a@student.american.edu>
2018-02-18 21:13:31 -0500

    Rearranged file structure

--- src/scanner.js
+++ src/scanner.js
@@ -3,22 +3,34 @@ import tokens from 'text!token-type.json'
 export class Scanner {
 
 	constructor() {
-    this.tokens = JSON.parse(tokens);
-    console.log(this.tokens);
+    // this.tokens = JSON.parse(tokens);
+    // console.log(this.tokens);
     
-    this.token_reg_exes = {};
-    for (let token_key in this.tokens) {
-      let token = this.tokens[token_key];
-      console.log(token);
+    // this.token_reg_exes = {};
+
+    // for (let token_key in this.tokens) {
+    //   let token = this.tokens[token_key];
+    //   console.log(token);
       
-      this.token_reg_exes[token["token"]] = new RegExp(token["reg_ex"]);
-    }
-    console.log(this.token_reg_exes);
+    //   this.token_reg_exes[token["token"]] = new RegExp(token["reg_ex"]);
+    // }
+    // for (let reg in this.token_reg_exes) {
+    //   console.log(reg);
+      
+      
+    // }
     
   }
   
-  Scan(token) {
-    console.log(token);
+  Scan(line) {
+    console.log(line.split(""));
+    for (let char of line.split("")) {
+      if (char == " ") {
+        console.log(char);
+      }
+    }
+    
+    console.log(line);
   }
 
   Error(line, message) {
