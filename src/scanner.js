335560aac8c5e09fe9cec935f021797d13be7470
asandhuzero <as7687a@student.american.edu>
2018-03-04 22:59:53 -0500

    Deleted some excess code.

--- src/scanner.js
+++ src/scanner.js
@@ -3,22 +3,6 @@ import tokens from 'text!token-type.json'
 export class Scanner {
 
 	constructor() {
-    // this.tokens = JSON.parse(tokens);
-    // console.log(this.tokens);
-    
-    // this.token_reg_exes = {};
-
-    // for (let token_key in this.tokens) {
-    //   let token = this.tokens[token_key];
-    //   console.log(token);
-      
-    //   this.token_reg_exes[token["token"]] = new RegExp(token["reg_ex"]);
-    // }
-    // for (let reg in this.token_reg_exes) {
-    //   console.log(reg);
-      
-      
-    // }
     // This will be the array of data.
     this.data_array = [];
     // This will be the array of code.
@@ -27,8 +11,6 @@ export class Scanner {
     this.labels = {};
     // This will be used for the directive mapping.
     this.directives = {};
-
-    
   }
   
   Scan(source_code_array) {
